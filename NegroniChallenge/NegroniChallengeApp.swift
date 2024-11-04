473456c0cd8d6c6cd0795540107296b0e0a3704b
Luca Maria Incarnato <lucaincarnato.03@gmail.com>
2024-11-04 12:48:59 +0100

    Initial Commit

---
+++ NegroniChallenge/NegroniChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NegroniChallengeApp.swift
+//  NegroniChallenge
+//
+//  Created by Luca Maria Incarnato on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct NegroniChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
