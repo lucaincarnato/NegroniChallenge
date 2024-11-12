39afed2952571c832125ab8f9dff4dfdea4a7b3e
Michele Barbato <michele_barbato_@libero.it>
2024-11-12 10:47:01 +0100

    added SideBar (LucaVaffanculo<3)

---
+++ NegroniChallenge/View/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  NegroniChallenge
+//
+//  Created by Luca Maria Incarnato on 04/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        // TabBar with Sidebar option to display main sections of the app
+        TabView{
+            Tab("Home", systemImage: "house"){
+                HomeView()
+            }
+            Tab("Search", systemImage: "magnifyingglass"){
+                SearchView()
+            }
+            // Show the link to each speech
+            TabSection("Speeches") {
+                Tab("All topics", systemImage: "pencil") {
+                    // For future: there goes the NavigationLink for the speech
+                    Text("This is the blog page")
+               }
+            }
+            .defaultVisibility(.hidden, for: .tabBar)
+            // Hides the Section from the TabBar but not from the Sidebar
+        }
+        .tabViewStyle(.sidebarAdaptable) // Allows the Sidebar
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NegroniChallenge/View/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  NegroniChallenge
+//
+//  Created by Michele Barbato on 12/11/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
